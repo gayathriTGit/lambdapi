dbf69e8d26a74047981cf097c082fb27468e11de
gayathriT <98060548+gayathriTGit@users.noreply.github.com>
2025-09-01 22:30:39 -0700

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,8 @@ import json
 
 def lambda_handler(event, context):
     # NYC coordinates
-    latitude = '40.71427'
-    longitude = '-74.00597'
+    latitude = '47.6740'
+    longitude = '122.1215'
     
     api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true&temperature_unit=fahrenheit"
     request = requests.get(api_url)
